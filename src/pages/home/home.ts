979f82b77a4ceca267cbdc6b3986a76b43e0e18e
Mrigesh08 <mrigeshmadaan@gmail.com>
2018-08-12 19:42:18 +0530

    added cards

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { MenuPage } from '../menu/menu';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,6 +9,9 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+  menuPage(){
+    this.navCtrl.push(MenuPage);
   }
 
 }
--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -14,8 +14,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'menu.html',
 })
 export class MenuPage {
-
+  items : any[];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.items = [
+      {
+        image : "imgs/Menu/yo.png",
+        price : "Rs. 250",
+        name  : "Name1",
+        description : "1. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      },
+      {
+        image : "imgs/Menu/yo.png",
+        price : "Rs. 550",
+        name  : "Name2",
+        description : "2. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+
+      }
+    ];
   }
 
   ionViewDidLoad() {
