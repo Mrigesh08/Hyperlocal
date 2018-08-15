d723c55ba79e5185aef5996b9abd188949d8593f
Mrigesh08 <mrigeshmadaan@gmail.com>
2018-08-15 11:27:40 +0530

    Tutorials completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,39 +5,29 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { IonicStorageModule, Storage } from '@ionic/storage';
 
 import { MyApp } from './app.component';
-// import { HomePage } from '../pages/home/home';
-
-export function createTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
+import { HomePage } from '../pages/home/home';
+import { TutorialPage} from "../pages/tutorial/tutorial";
 
 @NgModule({
   declarations: [
     MyApp,
-    // HomePage
+    HomePage,
+    TutorialPage
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: (createTranslateLoader),
-        deps: [HttpClient]
-      }
-    }),
     IonicModule.forRoot(MyApp),
     IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    // HomePage
+    HomePage,
+    TutorialPage
   ],
   providers: [
     StatusBar,
--- src/pages/tutorial/tutorial.module.ts
+++ src/pages/tutorial/tutorial.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-import { TutorialPage } from './tutorial';
-import { TranslateModule } from '@ngx-translate/core';
+import { TutorialPage } from "./tutorial";
 
 @NgModule({
   declarations: [
-    TutorialPage,
+    TutorialPage
   ],
   imports: [
     IonicPageModule.forChild(TutorialPage),
-    TranslateModule.forChild()
-  ],
-  exports: [
-    TutorialPage
   ]
 })
 export class TutorialPageModule { }
